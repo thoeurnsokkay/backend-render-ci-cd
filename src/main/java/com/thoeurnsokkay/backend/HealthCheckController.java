946e9710916f5cd09e8440929f7e442a62c93c41
thoeurnsokkay <thoeurnsokkay@gmail.com>
2026-02-28 19:05:37 +0700

    add health check controller

--- src/main/java/com/thoeurnsokkay/backend/HealthCheckController.java
+++ src/main/java/com/thoeurnsokkay/backend/HealthCheckController.java
@@ -1,4 +1,12 @@
 package com.thoeurnsokkay.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+    @GetMapping("/api/health")
+    public String healthCheck() {
+        return "OK";
+    }
 }
